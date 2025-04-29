7109e71a9d704167a0d4a45eb46273c4c40087f5
Muhammad Usman <6298674@edu.vaniercollege.qc.ca>
2025-04-29 14:43:29 -0400

    Bubble sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {0, 19, 2, 3, 421, 321, 1, 21};
+
+        System.out.println(Arrays.toString(bubbleSort(nums)));
+    }
+
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        return arr;
     }
 }
